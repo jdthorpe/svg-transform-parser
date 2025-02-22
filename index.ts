a81c1af329e3a345698ae1950fdf1e34ddb6e339
Jason Thorpe <jdthorpe@gmail.com>
2025-02-21 23:15:31 -0800

    types

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+export type {
+  point,
+  matrix,
+  translate,
+  scale,
+  rotate,
+  skewX,
+  skewY,
+  TRANSFORM,
+} from "./types";
 export {parse} from "./parse-svg-transform";
 export {Matrix, transform_to_matrix, invert, prod, apply} from "./src/matrix";
 export {render} from "./src/render";
